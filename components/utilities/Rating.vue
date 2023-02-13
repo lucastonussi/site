59ef6d5e3d378a3eb25d67c7b916868448846862
Marina Milhomens de Queiroz <marinamq@outlook.com>
2023-02-13 20:20:54 -0300

    rating created

---
+++ components/utilities/Rating.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <div class='rating'>
+    <h1>Avalie o conteúdo</h1>
+  </div>
+  <div class='emoji-button'>
+    <button @click="enviar('disgusted')">☹️</button>
+    <button @click="enviar('did_not_like')">🙁</button>
+    <button @click="enviar('neutral')">😐</button>
+    <button @click="enviar('liked')">🙂</button>
+    <button @click="enviar('really_liked')">😃</button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'avaliacao',
+  data () {
+    return {
+      rating: ''
+    }
+  },
+  methods: {
+    enviar(key) {
+      this.rating = key
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rating {
+margin-top: 15px;
+}
+
+.emoji-button {
+  margin-left: 50px;
+}
+</style>
