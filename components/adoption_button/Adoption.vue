dbb0cac94ddb84e4da1c6e8848ec3d8faed3cbf5
Marina Milhomens de Queiroz <marinamq@outlook.com>
2023-02-14 20:14:08 -0300

    added adoption component

---
+++ components/adoption_button/Adoption.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class='adoption'>
+      <button @click="enviar('adopt')">Audotar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'adoptpet',
+    data () {
+      return {
+        adoption: ''
+      }
+    },
+    methods: {
+      enviar(key) {
+        this.adoption = key
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.adoption {
+  font-size: 20px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  width: 40%;
+  height: 5vh;
+  text-align: center;
+  margin-left: 80px;
+}
+</style>
