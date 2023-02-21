6abc1d88e7ce4b71d26e6413133ee9b308cd7e4e
Marina Milhomens de Queiroz <marinamq@outlook.com>
2023-02-21 11:30:54 -0300

    adding component complet

--- components/adoption_button/Adoption.vue
+++ components/adoption_button/Adoption.vue
@@ -24,12 +24,9 @@
 
 <style scoped>
 .adoption {
-  font-size: 20px;
-  border: 1px solid grey;
+  font-size: 25px;
+  border: 5px outset grey;
   border-radius: 5px;
-  width: 40%;
-  height: 5vh;
   text-align: center;
-  margin-left: 80px;
 }
 </style>
